9b05869147aaf47ef1b2fa79682c4325cacb61d8
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-03-14 20:33:25 +0630

    get dogecoin data

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 fetch(
-  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=ffadggfhfdhdffdhf"
+  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
 )
   .then((res) => res.json())
   .then((data) => {
@@ -10,3 +10,12 @@ fetch(
     document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1502082553048-f009c37129b9?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NDE5NjAxMjV8&ixlib=rb-4.0.3&q=80&w=1080)`;
     document.getElementById("author").textContent = `By: Matheus Ferrero`;
   });
+
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data.name);
+    // console.log(data.image.large);
+    // console.log(data);
+  })
+  .catch((err) => console.error(err));
