9c947bf5e248ea62c5202d1c76de7724e35974d2
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-03-14 21:20:20 +0630

    get prices

--- index.js
+++ index.js
@@ -20,9 +20,15 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
     return res.json();
   })
   .then((data) => {
+    console.log(data);
     document.getElementById("crypto-top").innerHTML = `
     <img src=${data.image.small} />
     <span>${data.name}</span>
+    `;
+    document.getElementById("crypto").innerHTML += `
+      <p>🎯: $${data.market_data.current_price.usd}</p>
+      <p>👆: $${data.market_data.high_24h.usd}</p>
+      <p>👇: $${data.market_data.low_24h.usd}</p>
     `;
   })
   .catch((err) => console.error(err));
