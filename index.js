1ce727f60a92d3b45f6e9b22dad27c1e8c3454ca
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-03-15 08:27:32 +0630

    add weather icon

--- index.js
+++ index.js
@@ -50,6 +50,12 @@ navigator.geolocation.getCurrentPosition((position) => {
       }
       return res.json();
     })
-    .then((data) => console.log(data))
+    .then((data) => {
+      console.log(data);
+      const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+      document.getElementById("weather").innerHTML += `
+        <img src=${iconUrl} />
+      `;
+    })
     .catch((err) => console.error(err));
 });
