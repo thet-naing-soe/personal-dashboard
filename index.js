abe1512769098fa3dc430b67db43a1088d4a0261
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-03-15 09:44:28 +0630

    Update - using await

--- index.js
+++ index.js
@@ -1,36 +1,29 @@
-fetch(
+const res = await fetch(
   "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
-)
-  .then((res) => res.json())
-  .then((data) => {
-    document.body.style.backgroundImage = `url(${data.urls.regular})`;
-    document.getElementById("author").textContent = `By: ${data.user.name}`;
-  })
-  .catch((err) => {
-    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1502082553048-f009c37129b9?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NDE5NjAxMjV8&ixlib=rb-4.0.3&q=80&w=1080)`;
-    document.getElementById("author").textContent = `By: Matheus Ferrero`;
-  });
+);
+const data = await res.json();
+document.body.style.backgroundImage = `url(${data.urls.regular})`;
+document.getElementById("author").textContent = `By: ${data.user.name}`;
+// .catch((err) => {
+//   document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1502082553048-f009c37129b9?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NDE5NjAxMjV8&ixlib=rb-4.0.3&q=80&w=1080)`;
+//   document.getElementById("author").textContent = `By: Matheus Ferrero`;
+// });
 
-fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
-  .then((res) => {
-    if (!res.ok) {
-      throw Error("Something went wrong");
-    }
-    console.log(res.status);
-    return res.json();
-  })
-  .then((data) => {
-    document.getElementById("crypto-top").innerHTML = `
+const res = await fetch("https://api.coingecko.com/api/v3/coins/dogecoin");
+if (!res.ok) {
+  throw Error("Something went wrong");
+}
+const data = await res.json();
+document.getElementById("crypto-top").innerHTML = `
     <img src=${data.image.small} />
     <span>${data.name}</span>
     `;
-    document.getElementById("crypto").innerHTML += `
+document.getElementById("crypto").innerHTML += `
       <p>🎯: $${data.market_data.current_price.usd}</p>
       <p>👆: $${data.market_data.high_24h.usd}</p>
       <p>👇: $${data.market_data.low_24h.usd}</p>
     `;
-  })
-  .catch((err) => console.error(err));
+// .catch((err) => console.error(err));
 function getCurrentTime() {
   const now = new Date();
   const formattedTime = now.toLocaleTimeString("en-us", {
@@ -40,24 +33,19 @@ function getCurrentTime() {
 }
 setInterval(getCurrentTime, 1000);
 
-navigator.geolocation.getCurrentPosition((position) => {
-  fetch(
+navigator.geolocation.getCurrentPosition(async (position) => {
+  const res = await fetch(
     `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`
-  )
-    .then((res) => {
-      if (!res.ok) {
-        throw Error("Weather data is not available");
-      }
-      return res.json();
-    })
-    .then((data) => {
-      console.log(data);
-      const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-      document.getElementById("weather").innerHTML = `
-        <img src=${iconUrl} />
-        <p class="weather-temp">${Math.round(data.main.temp)}º</p>
-        <p class="weather-city">${data.name}</p>
-      `;
-    })
-    .catch((err) => console.error(err));
+  );
+  if (!res.ok) {
+    throw Error("Weather data is not available");
+  }
+  const data = await res.json();
+  const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  document.getElementById("weather").innerHTML = `
+    <img src=${iconUrl} />
+    <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+    <p class="weather-city">${data.name}</p>
+  `;
+  // .catch((err) => console.error(err));
 });
